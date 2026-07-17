3008531151e6f9affcb1b4d04eba4a9ad2b78d87
Azevd0 <davyson887@gmail.com>
2026-07-17 11:49:34 -0300

    refatorando o argumento de requisição para a Lambda para V2

--- src/main/java/br/com/davyson/userregistryapi/config/aws/StreamLambdaHandler.java
+++ src/main/java/br/com/davyson/userregistryapi/config/aws/StreamLambdaHandler.java
@@ -1,8 +1,8 @@
 package br.com.davyson.userregistryapi.config.aws;
 
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
-import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
@@ -13,11 +13,11 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class StreamLambdaHandler implements RequestStreamHandler {
-    private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+    private static final SpringBootLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handler;
 
     static {
         try{
-            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(UserregistryapiApplication.class);
+            handler = SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(UserregistryapiApplication.class);
         }catch(ContainerInitializationException containerEx){
             throw new RuntimeException("Não foi possível iniciar a aplicação"+ containerEx.getMessage());
         }
